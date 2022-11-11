6f0047f573b4693459ab721c3b61e20583d9806b
Lanak <lanak@lanakhive.com>
2022-11-11 14:17:00 -0600

    Switch build to CMake

--- buildfinal.bat
+++
@@ -1,3 +0,0 @@
-windres winresource.rc -o winresource.o
-gcc -m64 -DGLEW_STATIC -static main.c chroma.c chromagl.c util.c  winresource.o -O -fno-exceptions -I./inc -L./lib  -lmingw32 -lglew32 -lopengl32 -lSDL2main -lSDL2 -lgl-matrix -lole32 -loleaut32 -limm32 -lwinmm -lversion -luuid -ladvapi32 -lsetupapi -lshell32 -ldinput8 -mwindows -o chromadrencher.exe
-strip -s chromadrencher.exe
